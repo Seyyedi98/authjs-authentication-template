e8bf5253c97e918ffdd10d33a1b4a4730759f20f
Seyyedi <mr.seyyedi76@gmail.com>
2024-11-09 19:12:59 +0330

    changed some files name

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/client";
-import authConfig from "./auth-config";
+import authConfig from "./auth.config";
 
 export const {
   handlers: { GET, POST },
